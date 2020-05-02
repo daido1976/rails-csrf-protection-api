b8247ec400a08e6667fad6e53aaea5b02cbe5ee6
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-02 23:28:06 +0900

    Add login & logout routing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
+  def create
+    render plain: 'created!'
+  end
+
+  def destroy
+    render plain: 'destroyed!'
+  end
 end
