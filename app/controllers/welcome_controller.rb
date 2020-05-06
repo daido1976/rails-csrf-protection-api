67fe4192a96aea6843cb10dba2a1d20874cc624f
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-06 23:15:34 +0900

    Remove unused routing

--- app/controllers/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class WelcomeController < ApplicationController
-  def index
-    path = "#{Rails.public_path}/index.html"
-    render file: path
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'welcome#index'
-
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 end
