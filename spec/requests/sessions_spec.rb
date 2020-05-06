22b4dd2213cebf50648130124346d880c16dc4e2
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-06 23:17:48 +0900

    $ rubocop -a

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
-  describe "POST /login" do
-    it "is 200 ok" do
+RSpec.describe 'Sessions', type: :request do
+  describe 'POST /login' do
+    it 'is 200 ok' do
       post login_path
       expect(response).to have_http_status(200)
     end
 
-    it "saves user_id to session" do
+    it 'saves user_id to session' do
       post login_path
       expect(session[:user_id]).to eq('hoge_id')
     end
 
-    it "saves session" do
+    it 'saves session' do
       post login_path
       delete logout_path
       expect(session[:user_id]).to eq('hoge_id')
