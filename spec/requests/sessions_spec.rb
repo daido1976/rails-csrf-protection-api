22cb06805dbbb571bc4a46348e099e363f5dab5f
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-06 16:31:05 +0900

    Add spec to save session

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -6,5 +6,16 @@ RSpec.describe "Sessions", type: :request do
       post login_path
       expect(response).to have_http_status(200)
     end
+
+    it "saves user_id to session" do
+      post login_path
+      expect(session[:user_id]).to eq('hoge_id')
+    end
+
+    it "saves session" do
+      post login_path
+      delete logout_path
+      expect(session[:user_id]).to eq('hoge_id')
+    end
   end
 end
